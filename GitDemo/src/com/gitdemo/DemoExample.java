22a62b1af7c06f9ca542acd457d82eb8f8b58d72
Asmita <asmitasarwade7972@gmail.com>
2023-11-05 12:16:03 +0530

    print the value a and b

--- GitDemo/src/com/gitdemo/DemoExample.java
+++ GitDemo/src/com/gitdemo/DemoExample.java
@@ -3,10 +3,13 @@ package com.gitdemo;
 public class DemoExample {
 
 	public static void main(String[] args) {
+		
 		int a=10;
 		int b=20;
 		int c=a+b;
-		System.out.println("Addition is>>"+c);
+		System.out.println("value of a is>>"+a);
+		System.out.println("value of a is>>"+b);
+		//System.out.println("Addition is>>"+c);
 		
 	}
 
